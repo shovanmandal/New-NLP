5617cc17564d5141131feded88329aefe9f4dee8
shovanmandal <sushovan.93@gmail.com>
2017-12-28 20:47:02 +0530

    Update my_nlp.py

--- my_nlp.py
+++ my_nlp.py
@@ -1,32 +1,42 @@
 import nltk
+import pandas
 class my_nlp:
-     
+
+     def test(a):
+          alist=[]
+          for i in range(1,a+1):
+               string ='word'+str(i)
+               alist.append(string);
+          return alist
+          
      def My_NLP():
 
           file = open("Sample.txt", "r") 
           read_file = file.read()
-          #print(read_file)
 
           word_token = nltk.word_tokenize(read_file)
+          word_series = pandas.Series(word_token)
+          a=len(word_series)
+          #test_call= a.test(a);
+          table = pandas.DataFrame(word_series, index = a.test(a))
+          #print(table)
           word_pos = nltk.pos_tag(word_token)
+          
           #cnt = count(word_pos)
 
           #dist = map(dict, set(map(lambda x: tuple(x.item()), word_pos)))
           #print(dist)
           
-          for x in word_pos:
-               cnt = word_pos.count(x)
+          #for x in word_pos:
+               #cnt = word_pos.count(x)
                
-               print(x.unique(),':',cnt)
-               #print(x, )
+               #print(x.unique(),':',cnt)
                #print (" :")
                #print(cnt)
           #cnt = {x:word_pos.count(x)print('/n') for x in word_pos}
           #print d
-          #print(cnt ,"/n")
-          
-     
-     
+         #print(cnt ,"/n")
+
 
 my_nlp.My_NLP()
 
