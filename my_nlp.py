7afe66e37ae5f763e0fdd20be9a8d64d8abd3708
shovanmandal <sushovan.93@gmail.com>
2017-12-29 01:56:20 +0530

    Update my_nlp.py

--- my_nlp.py
+++ my_nlp.py
@@ -1,5 +1,6 @@
 import nltk
 import pandas
+import re
 
 class my_nlp:
 
@@ -11,34 +12,29 @@ class my_nlp:
           return alist
      
      def My_NLP(self):
-
           file = open("Sample.txt", "r") 
           read_file = file.read()
-
-          word_token = nltk.word_tokenize(read_file)
+          cleanString = re.sub('\W+',' ', read_file )
+          
+          word_token = nltk.word_tokenize(cleanString)
           word_series = pandas.Series(word_token)
-          
           a=len(word_series)
-          #print(word_series)
-          a_word={'one':word_series}
-          table = pandas.DataFrame(list(word_series), index = self.test(a))
-          print(table)
           word_pos = nltk.pos_tag(word_token)
+          newdict= {}
+          newwordlist=[]
+          newposlist=[]
+          for word,pos in word_pos:
+               #dictelement= word + ':' + pos
+               #print(dictelement)
+               newdict.update({word : pos})
+               newwordlist.append(word)
+               newposlist.append(pos)
+                    
+          data={'Words':newwordlist,'POS':newposlist}
+          table = pandas.DataFrame(data, index = self.test(a))
+          print(table)
           
-          #cnt = count(word_pos)
-
-          #dist = map(dict, set(map(lambda x: tuple(x.item()), word_pos)))
-          #print(dist)
           
-          #for x in word_pos:
-               #cnt = word_pos.count(x)
-               
-               #print(x.unique(),':',cnt)
-               #print (" :")
-               #print(cnt)
-          #cnt = {x:word_pos.count(x)print('/n') for x in word_pos}
-          #print d
-#print(cnt ,"/n")
 
 a = my_nlp()
 a.My_NLP()
